76ab19761ea99d36c388a151fdca61e66a71e52a
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-08-30 21:36:16 +0530

    processing

--- app/capture_test.py
+++ app/capture_test.py
@@ -2,15 +2,20 @@ import numpy as np
 from PIL import ImageGrab 
 import cv2 
 
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1 = 100 , threshold2 = 300)
+    return processed_img
+
+
 
 while(True):
 
     screen = ImageGrab.grab(bbox = (0, 40, 800, 640))
-    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype = 'uint8') \
-    # .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
-
-
-    cv2.imshow('window', cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB ))
+    
+    new_screen = process_img(np.array(screen))
+    cv2.imshow('window', new_screen)
+    # cv2.imshow('window', cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB ))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows() 
         break
