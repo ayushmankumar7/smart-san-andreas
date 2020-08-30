e355702be2f339a2a8d7e38671fba092072c1d0a
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-08-30 19:47:15 +0530

    capture test

--- app/capture_test.py
+++ app/capture_test.py
@@ -5,12 +5,13 @@ import cv2
 
 while(True):
 
-    printscreen_pil = ImageGrab.grab()
-    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype = 'uint8') \
-    .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+    screen = ImageGrab.grab(bbox = (0, 40, 800, 640))
+    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype = 'uint8') \
+    # .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
 
-    cv2.imshow('window', printscreen_numpy)
+
+    cv2.imshow('window', cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB ))
     if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destoryAllWindows() 
+        cv2.destroyAllWindows() 
         break
 
